c84dc0689ba4a6b671c3ff75edfba8c42263c5a2
aliali <aliali@test.com>
2025-04-22 19:19:55 +0300

    first

---
+++ src/main/java/com/example/dgh/DghApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DghApplication.class, args);
+    }
+
+}
